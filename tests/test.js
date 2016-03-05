daffc8d2cc2a1a76aaab46ebf3d88eda47b1f6fe
vrunoa <vruno.alassia@gmail.com>
2016-03-05 09:48:51 -0300

    add some tests

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,15 @@
 import { helper } from "../lib/"
+import uninstall from "../lib/"
 import chai from 'chai'
 let should = chai.should()
+let expect = chai.expect
 
 describe("Test lib", () => {
+  
+  it("test uninstall method is exported as default", (done) => {
+    expect(uninstall).to.be.a("function")
+    done()
+  })
   
   it("test packages are ordered alphabetically", (done) => {
     let devices = helper.adb.devices()
